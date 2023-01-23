cb3fae6d39b5c055dc04d780476838a31c2c2c8a
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-23 15:45:18 -0300

    primera ruta de la app

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express')
 //crear el servidor de express
 const app = express();
 
+//rutas
+app.get('/', (req, res)=>{
+    console.log('se requiere /')
+    res.json({
+        ok:true
+    })
+})
+
 //escuchar peticiones
 app.listen(4000,()=>{
     console.log(`Servidor corriendo en el puerto ${4000}`)
