f208c27c13a13a514c05c6b697ef6d7418c4a5ed
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-23 15:35:39 -0300

    servidor en le puerto 4000

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const express = require('express')
 
 //crear el servidor de express
 const app = express();
+
+//escuchar peticiones
+app.listen(4000,()=>{
+    console.log(`Servidor corriendo en el puerto ${4000}`)
+});
