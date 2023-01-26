022b0e0930d4347aff849bf6b41d6b378993e9af
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-26 16:12:00 -0300

    manejo de errors

--- middlewares/validate-fields.js
+++ middlewares/validate-fields.js
@@ -1,5 +1,18 @@
 const {response} = require('express');
+const {validationResult} = require('express-validator');
 
 const validateFields = (req, res = response, next)=>{
-    
+    const erros = validationResult(req);
+        if (!erros.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            erros: erros.mapped()
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validateFields,
 }
