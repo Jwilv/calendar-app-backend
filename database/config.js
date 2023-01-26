d4d69b81bb207d9ff714edb330adbdf11ca19550
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-26 18:44:15 -0300

    se creo la conexion a db

--- database/config.js
+++ database/config.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const dbConnection = async () => {
     try {
-        mongoose.connect('mongodb+srv://mern_user:iabwq7dXvCd5sgYy@cluster0.sxvlsh2.mongodb.net');
+        await mongoose.connect(process.env.DB_CNN);
+        console.log('DN online');
     } catch (error) {
-console.log(error)
-throw new Error('ERROR al inicializar DB')
+        console.log(error)
+        throw new Error('ERROR al inicializar DB')
     }
+}
+
+module.exports = {
+    dbConnection,
 }
