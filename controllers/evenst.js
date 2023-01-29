993b8aff97107754374480850796374b5cfdbade
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-28 23:58:08 -0300

    se crearon los controllers para events

---
+++ controllers/evenst.js
@@ -0,0 +1,37 @@
+const { response } = require('express')
+
+
+const getEvents = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'get event'
+    })
+}
+
+const newEvent = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'newEvent'
+    })
+}
+
+const modifyEvent = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'modifyEvent'
+    })
+}
+
+const deleteEvent = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'deleteEvent'
+    })
+}
+
+module.exports = {
+    getEvents,
+    newEvent,
+    modifyEvent,
+    deleteEvent,
+}
