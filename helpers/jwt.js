c64add6e188fd63fceb195e0878801b5bc7ab44c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-28 21:27:49 -0300

    se agrego tiempo de expiracion

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,9 @@ const generateJwt = (uid, name) => {
 
     return new Promise((resolve, reject) => {
         const payload = { uid, name }
-        jwt.sign(payload,process.env.SECRET_JWT_SEED)
+        jwt.sign(payload,process.env.SECRET_JWT_SEED,{
+            expiresIn:'2h'
+        })
     })
 
 }
