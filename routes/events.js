dd6b07e79a5a1e9dc1580fc1821aabd723fe1a8f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-29 12:55:31 -0300

    refactor

--- routes/events.js
+++ routes/events.js
@@ -7,14 +7,15 @@ const { Router } = require('express');
 const router = Router();
 const { validateJwt } = require('../middlewares/validate-jwt')
 const {getEvents,newEvent,modifyEvent,deleteEvent} = require('../controllers/evenst')
+router.use(validateJwt);
 //todas tienen que pasar por la validacion de jwt
 //obtener eventos
-router.get('/', validateJwt, getEvents)
+router.get('/', getEvents)
 //crear evento
-router.post('/', validateJwt, newEvent)
+router.post('/', newEvent)
 //modificar evento
-router.put('/:id', validateJwt, modifyEvent)
+router.put('/:id', modifyEvent)
 //eliminar evento
-router.delete('/:id', validateJwt, deleteEvent)
+router.delete('/:id', deleteEvent)
 
 module.exports = router;
