da1bbf072b510b67f41deaaf01d572e60471017a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-29 12:56:18 -0300

    format

--- routes/events.js
+++ routes/events.js
@@ -6,9 +6,9 @@ hots + /api/events
 const { Router } = require('express');
 const router = Router();
 const { validateJwt } = require('../middlewares/validate-jwt')
-const {getEvents,newEvent,modifyEvent,deleteEvent} = require('../controllers/evenst')
-router.use(validateJwt);
+const { getEvents, newEvent, modifyEvent, deleteEvent } = require('../controllers/evenst')
 //todas tienen que pasar por la validacion de jwt
+router.use(validateJwt);
 //obtener eventos
 router.get('/', getEvents)
 //crear evento
