2afe0e53bc8b9947643d5de57556c94f34056156
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-29 17:45:30 -0300

    se guarda event en db

--- controllers/evenst.js
+++ controllers/evenst.js
@@ -1,4 +1,5 @@
 const { response } = require('express')
+const Event = require('../models/Event')
 
 
 const getEvents = (req, res = response) => {
@@ -8,12 +9,22 @@ const getEvents = (req, res = response) => {
     })
 }
 
-const newEvent = (req, res = response) => {
-    console.log(req.body)
-    res.status(200).json({
-        ok: true,
-        msg: 'newEvent'
-    })
+const newEvent = async(req, res = response) => {
+    const event = new Event(req.body)
+    try {
+        event.user = req.uid;
+        const savedEvent = await  event.save();
+        res.json({
+            ok:true,
+            event : savedEvent,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg:'comuniquese con un administrador'
+        })
+    }
 }
 
 const modifyEvent = (req, res = response) => {
