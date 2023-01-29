dff62a26d6501dca77a6c7b631ab52ac2aafc35e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-28 21:27:12 -0300

    se agrego la variable de entorno al jwt

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const generateJwt = (uid, name) => {
 
     return new Promise((resolve, reject) => {
         const payload = { uid, name }
-        jwt.sign(payload,)
+        jwt.sign(payload,process.env.SECRET_JWT_SEED)
     })
 
 }
