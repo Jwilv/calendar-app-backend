5f5d889bab307b7555531b447d575fea219eaf5f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-28 21:20:51 -0300

    se creo el payload

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const generateJwt = ()=>{
+const generateJwt = (uid, name) => {
+
+    return new Promise((resolve, reject) => {
+        const payload = { uid, name }
+        jwt.sign(payload,)
+    })
 
 }
 
