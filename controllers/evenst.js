f34554e546ae73b7ffe9c1cbdbe66cd2a64c348b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-29 18:53:02 -0300

    se modifica el event

--- controllers/evenst.js
+++ controllers/evenst.js
@@ -5,7 +5,7 @@ const Event = require('../models/Event')
 const getEvents = async (req, res = response) => {
     // esto es para ver solo los eventos del usuario activo
     // const evenst = await Event.find({user:req.uid}).populate('user','name')
-    const evenst = await Event.find().populate('user','name')
+    const evenst = await Event.find().populate('user', 'name')
     res.status(200).json({
         ok: true,
         msg: 'get event',
@@ -31,22 +31,32 @@ const newEvent = async (req, res = response) => {
     }
 }
 
-const modifyEvent = async(req, res = response) => {
+const modifyEvent = async (req, res = response) => {
     const eventId = req.params.id
+    const { uid } = req
     try {
         const event = await Event.findById(eventId)
-        if(!event){
+        if (!event) {
             return res.status(404).json({
-                ok:false,
-                msg:"ningun evento con ese Id"
+                ok: false,
+                msg: "ningun evento con ese Id"
             })
         }
-        if(event.user.toString() !== uid){
+        if (event.user.toString() !== uid) {
             return res.status(401).json({
-                ok:false,
-                msg:'no tiene privilegio de editar este evento'
+                ok: false,
+                msg: 'no tiene privilegio de editar este evento'
             })
         }
+        const eventModified = {
+            ...req.body,
+            user: uid,
+        }
+        const eventUpdated = await Event.findByIdAndUpdate(eventId, eventModified, { new: true });
+        res.json({
+            ok: true,
+            event: eventUpdated,
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
