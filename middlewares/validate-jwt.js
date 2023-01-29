f44015b2e541e75031cc4e69ba2d8d47693a45cb
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-28 22:07:12 -0300

    se obtiene x-token

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -1,7 +1,11 @@
+const {response} = require('express')
 
 
-
-const validatejwt = ()=>{
+const validatejwt = (req, res = response, next)=>{
+//x-token
+const token = req.header('x-token')
+console.log(token)
+next();
 
 }
 
